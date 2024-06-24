9d0e6c540ff744bd0d28569993444f208114f484
Seongbin Kim <seongbin9786@gmail.com>
2024-06-24 22:09:56 +0900

    Docs: Adding View, Content decorator (WIP)

--- 1_component_lifecycle/src/README.md
+++ 1_component_lifecycle/src/README.md
@@ -14,3 +14,25 @@
 - afterContentChecked (반복)
 - afterViewChecked (반복)
 - onDestory
+
+```
+lifecycle: [constructor],       propValue: [undefined]
+lifecycle: [setter],            propValue: [value from parent!]
+lifecycle: [changes],           propValue: [value from parent!]
+lifecycle: [init],              propValue: [value from parent!]
+lifecycle: [doCheck],           propValue: [value from parent!]
+lifecycle: [contentInit],       propValue: [value from parent!]
+lifecycle: [contentChecked],    propValue: [value from parent!]
+lifecycle: [dom],               propValue: [value from parent!]
+
+lifecycle: [viewInit],          propValue: [value from parent!]
+lifecycle: [viewChecked],       propValue: [value from parent!]
+lifecycle: [dom],               propValue: [value from parent!]
+
+lifecycle: [doCheck],           propValue: [value from parent!]
+lifecycle: [contentChecked],    propValue: [value from parent!]
+lifecycle: [dom],               propValue: [value from parent!]
+
+lifecycle: [viewChecked],       propValue: [value from parent!]
+lifecycle: [dom],               propValue: [value from parent!]
+```
--- 1_component_lifecycle/src/components/child/child.component.ts
+++ 1_component_lifecycle/src/components/child/child.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChildren, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit, OnChanges, OnDestroy {
+export class ChildComponent implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterContentChecked, AfterViewInit, AfterViewChecked {
 
   myStateValue: string = '';
 
@@ -21,16 +21,39 @@ export class ChildComponent implements OnInit, OnChanges, OnDestroy {
     console.log(`scope: [${scope}], _prop: [${this._prop}], myStateValue: [${this.myStateValue}]`);
   }
 
-  constructor() {
+  constructor(private myRootElement: ElementRef) {
     this.log('constructor');
+    console.log('myRootElement:', myRootElement);
+  }
+
+  ngOnInit(): void {
+    this.log('init');
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.log('changes');
   }
 
-  ngOnInit(): void {
-    this.log('init');
+  ngDoCheck(): void {
+    this.log('doCheck')
+  }
+
+  ngAfterViewInit(): void {
+    this.log('viewInit');
+    console.log('ngAfterViewInit:', this.myRootElement);
+  }
+
+  ngAfterContentInit(): void {
+    this.log('contentInit');
+    console.log('ngAfterContentInit:', this.myRootElement);
+  }
+
+  ngAfterViewChecked(): void {
+    this.log('viewChecked');
+  }
+
+  ngAfterContentChecked(): void {
+    this.log('contentChecked');
   }
 
   ngOnDestroy(): void {
