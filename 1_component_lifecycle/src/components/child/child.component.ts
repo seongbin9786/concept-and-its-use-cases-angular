17851d4c9f7ce1241c8b0050b863015829f98c7f
Seongbin Kim <seongbin9786@gmail.com>
2024-06-26 22:25:09 +0900

    docs: Added ViewChild, ContentChild example code

--- 1_component_lifecycle/src/components/child/child.component.ts
+++ 1_component_lifecycle/src/components/child/child.component.ts
@@ -1,62 +1,110 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChildren, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from "@angular/core";
 
 @Component({
-  selector: 'child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"],
 })
-export class ChildComponent implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit, AfterContentChecked, AfterContentChecked, AfterViewInit, AfterViewChecked {
+export class ChildComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  @ViewChild("viewChild") element: ElementRef;
+  @ContentChild("child") contentElement: ElementRef;
 
-  myStateValue: string = '';
+  myStateValue: string = "";
 
   _prop: string;
 
   @Input()
   set prop(nextProp: string) {
     this._prop = nextProp;
-    this.log('setter');
+    this.log("setter");
   }
 
-  private log(scope: string) {
-    console.log(`scope: [${scope}], _prop: [${this._prop}], myStateValue: [${this.myStateValue}]`);
+  private log(scope: string, ...args: any[]) {
+    console.log(
+      `scope: [${scope}], _prop: [${this._prop}], myStateValue: [${this.myStateValue}], args: `,
+      args
+    );
   }
 
   constructor(private myRootElement: ElementRef) {
-    this.log('constructor');
-    console.log('myRootElement:', myRootElement);
+    this.log("constructor", myRootElement);
   }
 
   ngOnInit(): void {
-    this.log('init');
+    this.log("init");
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.log('changes');
+    this.log("changes");
   }
 
   ngDoCheck(): void {
-    this.log('doCheck')
+    this.log("doCheck");
   }
 
   ngAfterViewInit(): void {
-    this.log('viewInit');
-    console.log('ngAfterViewInit:', this.myRootElement);
-  }
-
-  ngAfterContentInit(): void {
-    this.log('contentInit');
-    console.log('ngAfterContentInit:', this.myRootElement);
+    this.log(
+      "after view init",
+      this.myRootElement,
+      this.element,
+      this.contentElement
+    );
   }
 
   ngAfterViewChecked(): void {
-    this.log('viewChecked');
+    this.log(
+      "after view checked",
+      this.myRootElement,
+      this.element,
+      this.contentElement
+    );
+  }
+
+  ngAfterContentInit(): void {
+    this.log(
+      "after content init",
+      this.myRootElement,
+      this.element,
+      this.contentElement
+    );
   }
 
   ngAfterContentChecked(): void {
-    this.log('contentChecked');
+    this.log(
+      "after content checked",
+      this.myRootElement,
+      this.element,
+      this.contentElement
+    );
   }
 
   ngOnDestroy(): void {
-    this.log('destory');
+    this.log("destory");
   }
 }
